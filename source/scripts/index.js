1d5ef0975ab2d0ac0d48485d6422282031e5bfed
sirorezzka <145597538+sirorezzka@users.noreply.github.com>
2024-06-03 23:03:56 +0300

    исправления 3 июня

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__button');
+const navList = document.querySelector('.nav__list');
+const map = document.querySelector('.contacts__map');
+
+nav.classList.remove('nav--nojs');
+navButton.classList.remove('nav__button--nojs');
+navList.classList.remove('nav__list--nojs');
+map.classList.remove('contacts__map--nojs');
